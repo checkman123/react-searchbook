caf39785421f52557fd6d6aa6099f008dfab35b4
Nez <checkman3335@gmail.com>
2022-05-08 22:22:24 -0700

    move fetchbook to searchBox

--- src/components/Home/HomePage/HomePage.js
+++ src/components/Home/HomePage/HomePage.js
@@ -12,12 +12,6 @@ import {
 } from "../../../redux/slices/searchbookSlice";
 
 const HomePage = () => {
-  //useDispatch to dispatch action, useSelector to get state from store
-  const dispatch = useDispatch();
-
-  const keyword = useSelector((state) => state.searchbookSlice.keyword);
-  const currentPage = useSelector((state) => state.searchbookSlice.currentPage);
-
   //totalPage:totalPage=totalItems/itemsPerPage, itemsPerPage = 5 ,totalItems,
   /*  at Pagination
       totalItems:10
@@ -26,43 +20,9 @@ const HomePage = () => {
   */
   //console.log("totalItems", totalItems);
 
-  useEffect(() => {
-    (async () => {
-      if (keyword === "") {
-        //Empty show search that have a in the book title
-        const result = await searchbook("a", currentPage, 5);
-        if (result?.data?.totalItems !== undefined) {
-          //setTotalItems(result.data.totalItems);
-          dispatch(setTotalItems(result.data.totalItems));
-        }
-        if (result?.data?.items !== undefined) {
-          //setItems(result.data.items);
-          dispatch(setSearchResult(result.data.items));
-        }
-      } else {
-        const result = await searchbook(keyword, currentPage, 5);
-        if (result?.data?.totalItems !== undefined) {
-          console.log("setTotalItems");
-          dispatch(setTotalItems(result.data.totalItems));
-        }
-        if (result?.data?.items !== undefined) {
-          console.log("setSearchResult");
-          dispatch(setSearchResult(result.data.items));
-        } else {
-          //No result found in search
-          dispatch(setSearchResult(null));
-        }
-
-        window.scrollTo(0, 0);
-      }
-    })();
-  }, [currentPage, keyword]);
-
   return (
     <>
       <Searchbox />
-
-      {}
       <Pagination itemsPerPage={5}>
         <div className="home__container">
           <SearchBar />
